527b28caa8c10d0a38cf896671e37126769e025f
Felipe <fescobar2002@gmail.com>
2021-07-29 00:57:22 -0300

    mensagem teste

---
+++ src/GerenciadorDeSalas.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.time.*;
+
+public class GerenciadorDeSalas {
+    
+    List<Sala> salas = new ArrayList<>(); 
+
+    public void adicionaSalaChamada(String nome, int capacidadeMaxima, String descricao) {
+
+    }
+
+    public void removeSalaChamada(String nomeDaSala) {
+
+    }
+
+    public List<Sala> listaDeSalas() {
+        return salas;
+    }
+
+    public void adicionaSala(Sala novaSala) {
+
+    }
+
+    //public Reserva reservaSalaChamada(String nomeDaSala, LocalDateTime dataInicial, LocalDateTime dataFinal) {
+
+    //}
+
+    public void cancelaReserva(Reserva cancelada) {
+        
+    }
+
+    //public Collection<Reserva> reservasParaSala(String nomeSala) {
+
+    //}
+
+    public void imprimeReservasDaSala(String nomeSala) {
+
+    }
+}
